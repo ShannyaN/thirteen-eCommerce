6efa963e0537832244df8476d8eee55cd5d25d55
ShannyaN <123843574+ShannyaN@users.noreply.github.com>
2023-04-21 02:02:41 -0400

    added tag data to routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,22 +1,30 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
+const seedTags = require('../../seeds/tag-seeds');
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-  Product.findAll().then((productData)=>{
-    res.json(productData)
-  })
-});
+    try{
+      const productData = await Product.findAll({
+        include:[{model:Category}, {model:Tag}],
+      });
+      res.status(200).json(productData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+    }
+  )
+// })
 
 // get one product
 router.get('/:id', async (req, res) => {
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category }],
+      include: [{ model: Category },  {model:Tag}],
     });
     res.status(200).json(productData);
   } catch (err) {
